aa5d7626ba637b87dde34d075f6b0f3c9134db99
Evans <pauliutsevans@gmail.com>
2024-05-28 16:53:09 +0300

    Day 1 - 1

---
+++ src/main/java/com/evansdev/book/BkSocialApiApplication.java
@@ -0,0 +1,13 @@
+package com.evansdev.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkSocialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkSocialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evansdev/book/BkSocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evansdev.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkSocialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

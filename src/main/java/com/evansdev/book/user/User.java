9e4ae938b9b397012b2e85d730468c28b5adc8e8
Evans <pauliutsevans@gmail.com>
2024-06-03 13:39:39 +0300

    Day 3 - 2

--- src/main/java/com/evansdev/book/user/User.java
+++ src/main/java/com/evansdev/book/user/User.java
@@ -1,5 +1,7 @@
 package com.evansdev.book.user;
 
+import com.evansdev.book.book.Book;
+import com.evansdev.book.history.BookTransactionHistory;
 import com.evansdev.book.role.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -45,6 +47,12 @@ public class User implements UserDetails, Principal {
 
     @ManyToMany(fetch = FetchType.EAGER)
     private List<Role> roles;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    @OneToMany(mappedBy = "user")
+    private List<BookTransactionHistory> histories;
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
